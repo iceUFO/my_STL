931f5d31b8b19ab38c3deac2f9e9836abda2d2f0
iceUFO <yyufonba@126.com>
2017-03-11 20:59:46 +0800

    construct

---
+++ my_STL/__Construct.h
@@ -0,0 +1,54 @@
+#pragma once
+#ifndef __CONSTRUCT_H
+#define __CONSTRUCT_H
+
+#include <new>            //placement new
+#include "__Type_traits.h"
+
+namespace my_STL
+{
+	//construct
+	template <typename T1, typename T2>
+	inline void construct(T1 *p, const T2 &value)
+	{
+		new(p) T1(value);
+	}
+
+	//destroy
+	template <typename T>
+	inline void destroy(T *pointer)
+	{
+		pointer->~T();
+	}
+
+	template <typename ForwardIterator>
+	inline void destroy(ForwardIterator first, ForwardIterator last)
+	{
+		__destory(first, last, value(first));
+	}
+
+	//ÅÐ¶ÏÐÍ±ð
+	template <typename ForwardIterator, typename T>
+	inline void __destroy(ForwardIterator first, ForwardIterator last, T*)
+	{
+		using trivial_destructor = __type_traits<T>::has_trivial_destructor;
+		__destroy_aux(first, last, trivial_destructor());
+	}
+
+	//has trivial destructor (false)
+	template <typename ForwardIterator>
+	inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __false_type)
+	{
+		for ( ; first < last; ++first)
+		{
+			destroy(&*first);
+		}
+	}
+
+	//has trivial destructor (true)
+	template <typename ForwardIterator>
+	inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __true_type) { }
+
+}
+
+#endif // !__CONSTRUCT_H
